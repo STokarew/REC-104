be76b9ed86c3ab41668ac88ed53e8aa263a3bc14
сергей токарев <sseregatokarew@gmail.com>
2022-04-26 19:47:29 +0700

    Добавьте файлы проекта.

---
+++ REC-104/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace REC_104
+{
+    public class Entity<T>
+    {
+        public T Data { get; }
+        public Entity(T data)
+        {
+            Data = data;
+        }
+    }
+
+    public class Entity2<T2>
+    {
+        public T2 Data2 { get; }
+        public Entity2(T2 Data2)
+        {
+            this.Data2 = Data2;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
